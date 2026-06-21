05cd29316903fdcf8f043e17310929ee6ffdf2cd
Shadie <florestefelvir18@gmail.com>
2026-06-20 22:03:04 -0600

    HomeController

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -1,8 +1,18 @@
 <?php
 namespace Controllers;
+use \Dao\Products\Products as ProductsDao;
+use \Views\Renderer as Renderer;
 
-class HomeController extends PublicController
+
+class Index extends PublicController
 {
-   
+    public function run() :void
+    {
+        $viewData = [];
+        $viewData["productsOnSale"] = ProductsDao::getDailyDeals();
+        $viewData["productsHighlighted"] = ProductsDao::getFeaturedProducts();
+        $viewData["productsNew"] = ProductsDao::getNewProducts();
+        Renderer::render("home", $viewData);
+    }
 }
 ?>
