cd8b3c4ee8a1fb070d8d21bd818e595f473857a8
Shadie <florestefelvir18@gmail.com>
2026-06-20 22:01:26 -0600

    More products

--- src/Dao/Poducts/Products.php
+++ src/Dao/Poducts/Products.php
@@ -26,5 +26,50 @@ class  Products {
           ]
         ];
     }
+
+    public static function getNewProducts() {
+      return [
+          [
+            "productId" => 99,
+            "productName" => "Producto 99",
+            "productDescription" => "Descripción del producto nuevo 99",
+            "productImgUrl" => "https://via.placeholder.com/150",
+            "productPrice" => 50.00
+          ],
+          [
+            "productId" => 100,
+            "productName" => "Producto 100",
+            "productDescription" => "Descripción del producto nuevo 100",
+            "productImgUrl" => "https://via.placeholder.com/150",
+            "productPrice" => 130.00
+          ]
+        ];
+    }
+
+    public static function getDailyDeals() {
+      return [ 
+          [
+            "productId" => 73,
+            "productName" => "Producto 73",
+            "productDescription" => "Descripción del producto 73",
+            "productImgUrl" => "https://via.placeholder.com/150",
+            "productPrice" => 10.00
+          ],
+          [
+            "productId" => 15,
+            "productName" => "Producto 15",
+            "productDescription" => "Descripción del producto 15",
+            "productImgUrl" => "https://via.placeholder.com/150",
+            "productPrice" => 13.00
+          ],
+          [
+            "productId" => 10,
+            "productName" => "Producto 10",
+            "productDescription" => "Descripción del producto 10",
+            "productImgUrl" => "https://via.placeholder.com/150",
+            "productPrice" => 20.00
+          ]
+        ];
+    }
 }
 ?>
